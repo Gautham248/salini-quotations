e9fc1bc5618af19e148fe8a863fc317ffc642093
Gautham248 <gkn240801@gmail.com>
2026-07-27 05:19:00 +0530

    added netlify config files

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,7 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["@prisma/adapter-libsql", "@prisma/client"],
+};
+
 export default nextConfig;
